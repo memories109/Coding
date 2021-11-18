d81c23895837fd8c80c19916ecbff58a8ff9a896
Ghosts <memories109@gmail.com>
2021-11-18 22:50:07 +0900

    question 2231

--- BruteForce/2021118/src/BruteForce/Question_2231.java
+++ BruteForce/2021118/src/BruteForce/Question_2231.java
@@ -1,11 +1,35 @@
 package BruteForce;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Question_2231 {
     
+
+    /**
+     * 
+     * 백준 Question 2231 분해합 문제 
+     * 
+     * 어떤 자연수 N이 있을 때, 그 자연수 N의 분해합은 N과 N을 이루는 각 자리수의 합을 의미한다. 
+     * 어떤 자연수 M의 분해합이 N인 경우, M을 N의 생성자라 한다. 
+     * 예를 들어, 245의 분해합은 256(=245+2+4+5)이 된다. 따라서 245는 256의 생성자가 된다. 
+     * 물론, 어떤 자연수의 경우에는 생성자가 없을 수도 있다. 반대로, 생성자가 여러 개인 자연수도 있을 수 있다.
+     * 
+     * 
+     * 
+     */
     public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        /**주어진 예제 문 
+         * 216일 경우 
+         *  216 = 216 + 2+4+6 
+         *  2021.11.19 다시 시작
+         * 
+         *         
+         */
 
-        System.out.print("11111");
+       
     }
-
-
 }
